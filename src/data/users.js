cbf001d4ab92930588b327fcb671f912b2ba4694
MrKrabsArt <kajkovartem20@gmail.com>
2025-05-25 21:38:15 +0700

    Add files via upload

--- src/data/users.js
+++ src/data/users.js
@@ -1,20 +1,38 @@
 export const users = [
     {
       id: 1,
-      username: "мама",
-      password: "мама123",
-      notes: ["Дорогая мама...", "Не забывай..."],
+      username: "Инопланетянин",
+      password: "universal_password",
+      notes: ["Привет незнакомец)))", "Возможно я просто не успел сделать для тебя аккаунт...Прости(" , "Но раз ты сюда зашел... мне очень приятно!)", "Будь здорв и найди свое счастье)", "И обязательно, занимайся тем чем нравится тебе...не слушай их, будь собой"],
+      img: [
+        "https://img.freepik.com/premium-vector/cute-alien-with-peace-hand-cartoon-vector-icon-illustration_480044-945.jpg",
+        "https://i.pinimg.com/736x/14/a2/9e/14a29eac68dca87516c7f41a92ffd2c1.jpg",
+        "https://i.pinimg.com/736x/0b/0b/18/0b0b185f0691c6e0711387ce287e7738.jpg",
+        "https://i.pinimg.com/736x/1e/bb/27/1ebb27454b2aa80907902e1843fb8d4b.jpg",
+        "https://i.pinimg.com/736x/06/c1/0b/06c10b513dc99d3c64df3c1f9d9cab3c.jpg",
+        "https://i.pinimg.com/736x/05/6e/73/056e735e7c10bafb8d8e83af0b883a30.jpg",
+        "https://i.pinimg.com/736x/88/8b/a9/888ba992b024f21cc4effe0645db95d6.jpg",
+        "https://i.pinimg.com/736x/9f/c9/e0/9fc9e0565e313731c992505d9dbf1e16.jpg",
+        "https://i.pinimg.com/736x/ef/39/e2/ef39e275dc1eecdb3555a42d9870c527.jpg",
+        "https://i.pinimg.com/736x/95/99/33/959933e62e325c77b88161d9cd7c9a96.jpg",
+        "https://i.pinimg.com/736x/e6/11/e0/e611e0ce8d15f0dde7df6ed4bcab87bf.jpg",
+        "https://i.pinimg.com/736x/3c/c9/02/3cc90232977c27908908fbc8f71ca9bd.jpg",
+        "https://i.pinimg.com/736x/b2/ad/b4/b2adb432ea6275142b7805b4ecb4751e.jpg",
+        "https://i.pinimg.com/736x/2c/b4/5d/2cb45d7c76be5f557b6f424a67d3f57e.jpg",
+        "https://i.pinimg.com/736x/3c/58/13/3c5813ce1d00b56e316ef30de15fa0e8.jpg",
+        "https://i.pinimg.com/736x/b3/39/37/b33937b16ab1f10e65ca8b84d52d099f.jpg",
+        "https://i.pinimg.com/736x/6d/62/bd/6d62bd693835b71841b10cbac1e16c6d.jpg",
+        "https://i.pinimg.com/736x/87/ea/d2/87ead2279e6afad37f95ae4bb18c12d3.jpg",
+        "https://i.pinimg.com/736x/2d/3f/97/2d3f97049e44fca5219b8f065cb1bfc2.jpg",
+        "https://i.pinimg.com/736x/99/0d/9d/990d9d976a8a75bf425eec0c9778605a.jpg",
+        "https://i.pinimg.com/736x/0b/31/79/0b3179e74792a725e3f8dfa23ef9a084.jpg",
+        "https://i.pinimg.com/736x/d2/73/37/d27337ce2c3c7dcb8060f8c8f7942fb3.jpg"]
     },
     {
       id: 2,
-      username: "папа",
-      password: "папа123",
-      notes: ["Дорогой папа...", "Проверяй..."],
-    },
-    {
-      id: 3,
-      username: "universal_user",
-      password: "universal_password",
-      notes: ["Тест", "Тест..."],
+      username: "Любимая",
+      password: "NasKajArt0860",
+      notes: ["Привет любимая!)", "Обожаю тебя очень сильно!", "А я уже по тебе скучаю(...", "Знай я в тебя верю и очень сильно горжусь!", "Скоро я вернусь..."],
+      img: []
     }
-  ];
+  ]
